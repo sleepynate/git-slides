c130a4200fdedd74af6a42eda785df55dd8d9640
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-15 12:22:58 -0400

    intro slide
    removing the "demo" part of the project, we will only use unit tests.

---
+++ src/Bowling.hs
@@ -0,0 +1,9 @@
+-- | http://codingdojo.org/cgi-bin/wiki.pl?KataBowling
+module Bowling where
+
+
+-----------------------------------------
+-- Solving the Bowling Kata in Haskell --
+-----------------------------------------
+
+--            A live demo.             --
