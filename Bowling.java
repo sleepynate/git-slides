b0def311fe5f991968b9be40f97dec3671f76f34
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-15 17:55:53 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -1,46 +1,19 @@
 import junit.framework.TestCase;
 
 public class BowlingKata extends TestCase {
-  private ScoreCard scoreCard;
-
   public BowlingKata(String testName) {
     super(testName);
   }
 
-  protected void setUp() throws Exception {
-    super.setUp();
-    scoreCard = new ScoreCard();
-  }
-
-  protected void tearDown() throws Exception {
-    super.tearDown();
-    scoreCard = null;
-  }
-
   public void testPerfectGame() {
-    for(int i=0; i<12; ++i) {
-      scoreCard.addRoll(10);
-    }
-    scoreCard.computeScore();
-    assertEquals(scoreCard.getScore(), 300);
+    assertEquals(scoreCard.score("XXXXXXXXXXXX"), 300);
   }
 
   public void testAllNines() {
-    for(int i=0; i<10; ++i) {
-      scoreCard.addRoll(9);
-      scoreCard.addRoll(0);
-    }
-    scoreCard.computeScore();
-    assertEquals(scoreCard.getScore(), 90);
+    assertEquals(scoreCard.score("9-9-9-9-9-9-9-9-9-9-"), 300);
   }
 
   public void testAllSpares() {
-    for(int i=0; i<10; ++i) {
-      scoreCard.addRoll(5);
-      scoreCard.addRoll(5);
-    }
-    scoreCard.addRoll(5);
-    scoreCard.computeScore();
-    assertEquals(scoreCard.getScore(), 150);
+    assertEquals(scoreCard.score("5/5/5/5/5/5/5/5/5/5/5"), 150);
   }
 }
