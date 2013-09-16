32aa82d1d98258ae66f215542f728bf3ec978238
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-16 08:02:48 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -7,7 +7,7 @@
 // 
 // - Easier to test                                    (no setup)
 // - Easier to reason about behaviour                (no globals)
-// 
+// - Easier to reason about the impact of changes      (no order)
 // 
 // 
 // Disadvantages:
