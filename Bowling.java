b67eb1a839cb3f04955c24ddd36fc4d496c4d011
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-15 22:37:32 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -9,9 +9,15 @@ public class SeasonListAdapter {
   public void onEpisodesLoaded(EpisodeData data) {
     removeEmptySeasons(data);
     
-    Season season = getCurrentSeason();
+    Season season = getCurrentSeason(); // array out of bounds!
     displaySeason(season, data);
   }
   
-  // ...
+  private Season getCurrentSeason() {
+    return mSeasons[mCurrentSeason];
+  }
+  
+  private   void   removeEmptySeasons(EpisodeData data) {
+    mSeasons = /* ... */;
+  }
 }
