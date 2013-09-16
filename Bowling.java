9c60726590b4eb427a8d4d3bf0efaed9fccde31c
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-16 08:02:48 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -1,23 +1,18 @@
-import junit.framework.TestCase;
 
-// What an immutable version would look like:
+
+// IMMUTABILITY
 // 
-
-public class SeasonListAdapter {
-  // ...
-  
-  public SeasonListAdapter onEpisodesLoaded(EpisodeData data) {
-    Season[] seasons = removeEmptySeasons(data);
-    
-    Season season = getCurrentSeason(); // array index ok
-    displaySeason(season, data);
-    return new SeasonListAdapter(seasons);
-  }
-  private Season getCurrentSeason() {
-    return mSeasons[mCurrentSeason];
-  }
-  
-  private Season[] removeEmptySeasons(EpisodeData data) {
-    return     /* ... */;
-  }
-}
+// 
+// Advantages:
+// 
+// 
+// 
+// 
+// 
+// 
+// Disadvantages:
+// 
+// 
+// 
+// 
+// 
