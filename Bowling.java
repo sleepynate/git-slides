4dc0ca4f675a2f47113b0066092f45abc19433c8
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-17 21:53:46 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -1,18 +1,18 @@
 
 
-// IMMUTABILITY
-// 
-// 
-// Advantages:
-// 
-// - Easier to test                                    (no setup)
-// - Easier to reason about behaviour                (no globals)
-// - Easier to reason about the impact of changes      (no order)
-// - Easier to write parallel programs       (no race conditions)
-// 
-// Disadvantages:
-// 
-// - immutability is visible in the API     (not an impl. detail)
-// - not compatible with existing libraries     (the GUI debacle)
-// - need new immutable datastructures  (slower big-O complexity)
-// - need new immutable algorithms          (new way of thinking)
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                                               
+//                                          (new way of thinking)
